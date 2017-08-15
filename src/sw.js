43c89a817d96013a99b0db4820ea2b795619a071
Jonas Lundeland <jlundela@gmail.com>
2017-08-15 19:39:38 +0200

    Cache employee data and implement cache-first-then-network

--- src/sw.js
+++ src/sw.js
@@ -1,9 +1,19 @@
-const APP_SHELL_CACHE = 'app-shell-v1';
+/* Les meir om Service Worker APIet på Mozilla sine sider:
+ * https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API
+ */
+
+/* Nøklar som vil bli brukt i Cache Storage
+ * Les meir om Cache APIet her: https://developer.mozilla.org/en-US/docs/Web/API/Cache
+ */
+
+const FILE_CACHE = 'app-shell-v1';
 const DATA_CACHE = 'employees-v1';
 
-const offlineResources = [
+const appShellFiles = [
+  '/',
   'index.html',
   'offline.html',
+  'manifest.json',
   'app.js',
   'icons/sonat_200x200.png',
   'icons/sonat_400x400.jpg',
@@ -11,9 +21,14 @@ const offlineResources = [
   'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/css/materialize.min.css'
 ];
 
+const apiUrl = 'https://sonat-fagdag-pwa.firebaseio.com/employees.json';
+
 const cacheOfflineResources = () =>
-  caches.open(APP_SHELL_CACHE)
-    .then(cache => cache.addAll(offlineResources));
+  Promise.all([caches.open(FILE_CACHE), caches.open(DATA_CACHE)])
+    .then(([fileCache, dataCache]) => {
+      fileCache.addAll(appShellFiles);
+      dataCache.add(apiUrl);
+    });
 
 const handleOffline = e => () => {
   if (e.request.mode === 'navigate')
@@ -22,25 +37,40 @@ const handleOffline = e => () => {
 
 const getCachedFiles = e =>
   caches.match(e.request)
-    .then(response =>
-      response || fetch(e.request))
+    .then(response => response || fetch(e.request))
     .catch(handleOffline(e))
 
-const onInstall = (e) => {
-  console.log('[sw] - install');
+const fetchEmployeeData = request =>
+    Promise.all([caches.open(DATA_CACHE), fetch(request)])
+      .then(([cache, response]) => {
+        cache.put(request.url, response.clone());
+        return response;
+      })
+      .catch(error => { /* No internet connection */ });
+
+const cacheFirstThenNetwork = e =>
+  caches.match(e.request)
+    .then(response => {
+      setTimeout(() => fetchEmployeeData(e.request), 0);
+      return response;
+    });
+
+/* Lytt på install event
+ * https://developer.mozilla.org/en-US/docs/Web/API/InstallEvent
+ */
+
+self.addEventListener('install', (e) => {
   e.waitUntil(cacheOfflineResources());
-};
+});
 
-const onFetch = (e) => {
-  console.log('[sw] - fetch');
-  e.respondWith(getCachedFiles(e));
-};
+/* Lytt på fetch event
+ * https://developer.mozilla.org/en-US/docs/Web/API/FetchEvent
+ */
 
-const onActivate = (e) => {
-  console.log('[sw] - activate');
-};
-
-/* Registerer event listeners */
-self.addEventListener('install', onInstall);
-self.addEventListener('fetch', onFetch);
-self.addEventListener('activate', onActivate);
+self.addEventListener('fetch', (e) => {
+  if (e.request.url === apiUrl) {
+    e.respondWith(cacheFirstThenNetwork(e));
+  } else {
+    e.respondWith(getCachedFiles(e));
+  }
+});
