49d5c47f20673ef6d10a5fdecdf9cbb5333a6b53
Jonas Lundeland <jlundela@gmail.com>
2017-08-24 17:50:45 +0200

    Step back: Tar vekk caching av app shell

--- src/sw.js
+++ src/sw.js
@@ -2,35 +2,12 @@
  * https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API
  */
 
-/* Nøklar som vil bli brukt i Cache Storage
- * Les meir om Cache APIet her: https://developer.mozilla.org/en-US/docs/Web/API/Cache
- */
-
-const FILE_CACHE = 'app-shell-v1';
-
-const appShellFiles = [
-  '/offline.html',
-  '/manifest.json',
-  '/app.js',
-  '/icons/sonat_200x200.png',
-  '/icons/sonat_400x400.jpg',
-  '/icons/sonat_32x32.png',
-  '/style.css',
-  '/cards.css'
-];
-
-const cacheOfflineResources = () =>
-  Promise.all([caches.open(FILE_CACHE)])
-    .then(([fileCache]) => {
-      fileCache.addAll(appShellFiles);
-    });
-
 /* Lytt på install event
  * https://developer.mozilla.org/en-US/docs/Web/API/InstallEvent
  */
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(cacheOfflineResources());
+  /* Hint: Du må åpne cache index og legge til filene i cache. */
 });
 
 /* Lytt på fetch event
@@ -38,5 +15,5 @@ self.addEventListener('install', (e) => {
  */
 
 self.addEventListener('fetch', (e) => {
-  /* Hint: Dersom request fins i cache, returner direkte fra cache, ellers lar vi requesten gå gjennom */
+
 });
